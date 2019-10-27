55ff5266f12bb1e559b944915be15ba522c8d3ff
Phil Newman <pnewman@viimed.com>
2019-10-27 00:56:22 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,6 @@ veg2 = "macaroni"
 protein = "tofu"
 
 def meal_choice (veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
